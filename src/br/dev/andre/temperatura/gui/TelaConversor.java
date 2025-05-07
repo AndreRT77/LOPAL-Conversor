14336e4b43d8bf25e1a981e30527fcf8171b92f1
ANDRÉ ROBERTO TAVARES <25132696@dsn127m1246729.cfp127aluno.local>
2025-05-07 10:34:04 -0300

    Terminando a funcionalidade dos botões, adicionando fonte, e finalizando o projeto

--- src/br/dev/andre/temperatura/gui/TelaConversor.java
+++ src/br/dev/andre/temperatura/gui/TelaConversor.java
@@ -4,6 +4,7 @@ import br.dev.andre.temperatura.model.Temperatura;
 
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -32,32 +33,55 @@ public class TelaConversor {
 		tela.setResizable(false);
 		tela.setLayout(null);
 		
+		// Obtendo a referência do painel de conteúdo da tela
+
 		Container container = tela.getContentPane();
 		
+		//Criando a fonte para utilizar no resultado e no erro
+		
+		Font fonteResultado = new Font ("Arial black", Font.BOLD, 20);
+		Font fonteErro = new Font("Arial", Font.ITALIC, 15);
+		
+		// criando o label Celsius
+
 		labelCelsius = new JLabel();
-		labelCelsius.setText("Temperatura em graus celsius");
+		labelCelsius.setText("Temperatura em graus celsius:");
 		labelCelsius.setBounds(20, 20, 200, 30);
 		
+		// criando o textfield Celsius
+
 		textCelsius = new JTextField();
 		textCelsius.setBounds(20, 50, 410, 35);
 		
+		// criando o label Resultados
+
 		labelResultado = new JLabel();
-		labelResultado.setText("Resultado");
-		labelResultado.setBounds(150, 120, 240, 50);
-//		
+		labelResultado.setFont(fonteResultado);
+		labelResultado.setText("Resultado: ");
+		labelResultado.setBounds(150, 120,  410, 50);
+
+		// criando o label Mensagem para erros
+
 		labelMensagemErro = new JLabel();
-		labelMensagemErro.setText("ERRO");
+		labelMensagemErro.setFont(fonteErro);
+		labelMensagemErro.setText("Digite apenas números!");
 		labelMensagemErro.setForeground(Color.red);
-		labelMensagemErro.setBounds(150, 140, 150, 80);
+		labelMensagemErro.setBounds(160, 150,  410, 80);
 		
+		// criando o botão para converter para kelvin
 		buttonKelvin = new JButton();
 		buttonKelvin.setText(" Kelvin");
-		buttonKelvin.setBounds(230, 90, 200, 40);
+		buttonKelvin.setBounds(20, 90, 200, 40);
 		
+		// criando o botão para converter para Fahrenheit 
+
 		buttonFahrenheit = new JButton();
+		buttonFahrenheit.setBackground(Color.blue);
 		buttonFahrenheit.setText("Fahrenheit");
-		buttonFahrenheit.setBounds(20, 90, 200, 40);
+		buttonFahrenheit.setBounds(230, 90, 200, 40); 
 		
+		// Adicionando os componentes no painel de conteúdo do Jframe
+
 		container.add(labelCelsius);
 		container.add(textCelsius);
 		container.add(labelResultado);
@@ -65,31 +89,68 @@ public class TelaConversor {
 		container.add(buttonFahrenheit);
 		container.add(buttonKelvin);
 		
+		labelResultado.setVisible(false);
+        labelMensagemErro.setVisible(false);
+
+		// criando as funcionalidades do botãp do Kelvin / adicionando ouvintes de ação ao botào Kelvin
+
 	buttonKelvin.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				String Celsius= textCelsius.getText();
-				
-				
-				double celsiusdouble = Double.parseDouble(Celsius);
+				try {
+	                String celsius = textCelsius.getText();
+	                labelMensagemErro.setVisible(false);
 
+	                double celsiusDouble = Double.parseDouble(celsius);
 
-				Temperatura temperatura   = new Temperatura();
-				double temperaturaKelvin = temperatura.converterKelvin();
-				temperatura.getCelsius();
-				temperatura.setKelvin();
-				
-				String[] temperaturaResult = temperatura.mostrarTemperatura();
-				
-				listTemperatura.setListData(temperaturaResult);
-				
-				temperatura.mostrarTemperatura();
+	                Temperatura temperatura = new Temperatura();
+	                temperatura.setCelsius(celsiusDouble);
+	                double kelvin = temperatura.converterKelvin();
+	                String resultado = kelvin + " Kelvin ";
+	                
+	                labelResultado.setText(String.format("%.2f", kelvin) + " Kelvin");
+	                labelResultado.setVisible(true);
+	                
+	            } 
+					// Para caso o usuário digitar algo que não seja um número irá aparecer uma mensagem de erro
+
+					catch (NumberFormatException ex) {
+	                labelMensagemErro.setVisible(true);
+	                labelResultado.setVisible(false);
+	            }
 			}
 		
 		});
-	
+	//Criando as funcionalidades do botãp do Fahrenheit / adicionando ouvintes de ação ao botào Fahrenheit
+
+	buttonFahrenheit.addActionListener(new ActionListener() {
 		
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			try {
+                String celsius = textCelsius.getText();
+
+                double celsiusDouble = Double.parseDouble(celsius);
+
+                Temperatura temperatura = new Temperatura();
+                temperatura.setCelsius(celsiusDouble);
+                double fahrenheit = temperatura.converterFahrenheit();
+                String resultado = fahrenheit + " Fahrenheit";
+                
+                labelMensagemErro.setVisible(false);
+                labelResultado.setText(String.format("%.2f", fahrenheit)+ " Fahrenheit");
+                labelResultado.setVisible(true);
+            } 
+				// Para caso o usuário digitar algo que não seja um número irá aparecer uma mensagem de erro
+				catch (NumberFormatException ex) {
+                labelMensagemErro.setVisible(true);
+                labelResultado.setVisible(false);
+            }
+        }
+    });
+	
 		tela.setVisible(true);
 	}
 	
--- src/br/dev/andre/temperatura/model/Temperatura.java
+++ src/br/dev/andre/temperatura/model/Temperatura.java
@@ -1,5 +1,6 @@
 package br.dev.andre.temperatura.model;
 
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -17,31 +18,20 @@ public class Temperatura {
 		this.celsius = celsius;
 	}
 
-
+// Cálculo para converter graus celsius em fahrenheit
 	public double converterFahrenheit(){
 		double Fahrenheit = celsius * 1.8 + 32;
 		return Fahrenheit;
 	}
+// Cálculo para converter graus celsius em kelvin
 	public double converterKelvin(){
 		double Kelvin = celsius + 273.15;
 		return Kelvin;
 		
-		
-
-			
-
-		
-	
-	
 	}
 
-	public void setKelvin() {
-		// TODO Auto-generated method stub
-		
-	}
 
 	public String[] mostrarTemperatura() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 }
